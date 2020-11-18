8d436a4898bcadf55968f5619dc1a46eacf327c7
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-11-18 17:11:37 -0500

    update for models in the new setup

--- om_run_gpt_brainscore.sh
+++ om_run_gpt_brainscore.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
-#SBATCH --job-name=arch_search
-#SBATCH --array=0-9
-#SBATCH --time=56:00:00
+#SBATCH --job-name=neural_bench
+#SBATCH --array=0-31
+#SBATCH --time=96:00:00
 #SBATCH -c 16
-#SBATCH --mem=267G
+#SBATCH --mem=160G
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for benchmark in Fedorenko2016v3-encoding-weights Pereira2018-encoding-weights ; do
-for model in gpt2 transfo-xl-wt103 roberta-base xlm-mlm-xnli15-1024 albert-base-v2  ; do
-  model_list[$i]="$model"
-  benchmark_list[$i]="$benchmark"
-  i=$i+1
-done
+for benchmark in Fedorenko2016v3-encoding-weights \
+    Pereira2018-encoding-weights \
+    Fedorenko2016v3-encoding ; do
+      for model in glove \
+        lm_1b \
+        bert-large-uncased-whole-word-masking \
+        xlm-mlm-en-2048 \
+        t5-3b \
+        ctrl \
+        albert-xxlarge-v2 \
+        gpt2-xl ; do
+          model_list[$i]="$model"
+          benchmark_list[$i]="$benchmark"
+          i=$i+1
+      done
 done
 
 module add openmind/singularity
