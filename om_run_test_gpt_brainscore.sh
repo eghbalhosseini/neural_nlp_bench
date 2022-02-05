cc01ae79e23f53d53858dae1a4c2fe5df58ba3e4
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-02-05 15:06:13 -0500

    bugfix

--- om_run_test_gpt_brainscore.sh
+++ om_run_test_gpt_brainscore.sh
@@ -18,10 +18,10 @@ for benchmark in Fedorenko2016v3-encoding ; do
 done
 
 module add openmind/singularity
-export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
-RESULTCACHING_HOME=/om/user/`whoami`/.result_caching
+export SINGULARITY_CACHEDIR=/om2/user/`whoami`/st/
+RESULTCACHING_HOME=/om2/user/`whoami`/.result_caching
 export RESULTCACHING_HOME
-XDG_CACHE_HOME=/om/user/`whoami`/st
+XDG_CACHE_HOME=/om2/user/`whoami`/st
 export XDG_CACHE_HOME
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
