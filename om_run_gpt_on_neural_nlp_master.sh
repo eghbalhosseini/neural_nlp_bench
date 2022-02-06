0222d00299b67c3384b6df087674cdd6531fcc7d
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-02-06 06:58:01 -0500

    bugfix

--- om_run_gpt_on_neural_nlp_master.sh
+++ om_run_gpt_on_neural_nlp_master.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=neural_bench
-#SBATCH --array=0-1
+#SBATCH --job-name=nlp_master
+#SBATCH --array=0-7
 #SBATCH --time=96:00:00
 #SBATCH -c 16
 #SBATCH --mem=160G
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
+#SBATCH --output=R-%x.%j.out
 
 i=0
-for benchmark in Fedorenko2016v3-encoding ; do
-      for model in distilgpt2 ; do
+for benchmark in Fedorenko2016v3-encoding Pereira2018-encoding ; do
+      for model in distilgpt2 distilgpt2-untrained gpt2 gpt2-untrained ; do
           model_list[$i]="$model"
           benchmark_list[$i]="$benchmark"
           i=$i+1
