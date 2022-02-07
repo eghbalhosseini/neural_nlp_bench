000c23472fe42fa5cce1686470356e3d53388965
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-02-07 10:20:37 -0500

    fixing pytorch incompatible models save

---
+++ fix_pytorch_version.py
@@ -0,0 +1,12 @@
+# import numpy as np
+import os
+import argparse
+import torch
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('ckpt_dir', type=str, default='',
+                    help='subject ID, e.g. "sub190" to run this script on')
+args = parser.parse_args()
+if __name__ == '__main__':
+    ckpt_dir = args.ckpt_dir
+    model_w = torch.load(f"{ckpt_dir}/pytorch_model.bin",map_location=torch.device('cpu'))
+    torch.save(model_w, f"{ckpt_dir}/pytorch_model_1_4.bin",_use_new_zipfile_serialization=False)
