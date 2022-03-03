a6a6a766897bd114b92bc74df75b53d818813204
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-03-03 11:08:54 -0500

    mock test

--- om_run_gpt_neox_pos_learned_neural_nlp_2022.sh
+++ om_run_gpt_neox_pos_learned_neural_nlp_2022.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=nlpGptNeoX
+#SBATCH --job-name=nlp2022
 #SBATCH --array=0-7
 #SBATCH --time=56:00:00
 #SBATCH -c 16
@@ -7,13 +7,11 @@
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
-#SBATCH --output=R-%x.%j.out
 
 i=0
 for benchmark in Fedorenko2016v3-encoding Pereira2018-encoding ; do
   for model in gpt2-neox-pos_learned-100M-v2 gpt2-neox-pos_learned-100M-v2-untrained \
                 gpt2-neox-pos_learned-1B-v2 gpt2-neox-pos_learned-1B-v2-untrained ; do
-
             model_list[$i]="${model}"
             benchmark_list[$i]="$benchmark"
             i=$[$i+1]
@@ -22,7 +20,7 @@ done
 
 module add openmind/singularity
 export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
-RESULTCACHING_HOME=/om/user/`whoami`/.result_caching
+RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching
 export RESULTCACHING_HOME
 XDG_CACHE_HOME=/om/user/`whoami`/st
 export XDG_CACHE_HOME
@@ -30,4 +28,8 @@ echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running benchmark ${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
 
-singularity exec -B /om:/om /om/user/`whoami`/simg_images/neural_nlp_master_fz.simg python /om/user/ehoseini/neural-nlp-master/neural-nlp/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
+. ~/.bash_profile
+. ~/.bashrc
+conda activate neural_nlp_2022
+
+/om/user/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om/user/ehoseini/neural-nlp-2022/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
