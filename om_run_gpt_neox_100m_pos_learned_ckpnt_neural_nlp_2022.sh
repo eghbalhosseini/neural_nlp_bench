a38ec340d05cf3a2f43e7321c22397fa74b7f9a4
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-05-09 10:05:28 -0400

    running diffferent initialzation

---
+++ om_run_gpt_neox_100m_pos_learned_ckpnt_neural_nlp_2022.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=nlp2022
+#SBATCH --array=0-2
+#SBATCH --time=56:00:00
+#SBATCH --mem=120G
+#SBATCH --exclude node017,node018
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ehoseini@mit.edu
+
+i=0
+for benchmark in Fedorenko2016v3-encoding Pereira2018-encoding ; do
+  for model in gpt2-neox-pos_learned-100M-v2-ckpnt  ; do
+      for checkpoint in `seq 2900 100 3000`; do
+            model_list[$i]="${model}-${checkpoint}"
+            benchmark_list[$i]="$benchmark"
+            i=$[$i+1]
+      done
+    done
+done
+
+module add openmind/singularity
+export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
+RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching
+export RESULTCACHING_HOME
+XDG_CACHE_HOME=/om/user/`whoami`/st
+export XDG_CACHE_HOME
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running benchmark ${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
+
+. ~/.bash_profile
+. ~/.bashrc
+conda activate neural_nlp_2022
+
+/om/user/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om/user/ehoseini/neural-nlp-2022/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
