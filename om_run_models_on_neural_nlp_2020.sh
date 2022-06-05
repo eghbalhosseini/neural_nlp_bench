93088cbde84aff8f7dfbe9f3aafe5bb4e6fafe7c
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-06-05 14:20:56 -0400

    bugfix

--- om_run_models_on_neural_nlp_2020.sh
+++ om_run_models_on_neural_nlp_2020.sh
@@ -19,7 +19,7 @@ done
 
 module add openmind/singularity
 export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
-RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching
+RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching_2020
 export RESULTCACHING_HOME
 XDG_CACHE_HOME=/om/user/`whoami`/st
 export XDG_CACHE_HOME
@@ -31,4 +31,4 @@ echo "cache id " $RESULTCACHING_HOME
 
 
 
-singularity exec -B /om:/om /om/user/`whoami`/simg_images/neural_nlp_master_fz.simg python /om/user/ehoseini/neural-nlp-master/neural-nlp/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
+singularity exec -B /om:/om,/om2:/om2,/om5:/om5 /om/user/`whoami`/simg_images/neural_nlp_master_fz.simg python /om/user/ehoseini/neural-nlp-master/neural-nlp/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
