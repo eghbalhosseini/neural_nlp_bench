f00c0cb371d48f4cd3573a36a6d69639f0cc3a33
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-06-10 13:16:08 -0400

    testing neural nlp

---
+++ om_run_ANN_models_on_neural_nlp_2022.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --job-name=nlp2022
+#SBATCH --array=0-6
+#SBATCH --time=56:00:00
+#SBATCH -c 16
+#SBATCH --mem=160G
+#SBATCH --exclude node017,node018
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ehoseini@mit.edu
+
+i=0
+for benchmark in Pereira2018-encoding  ; do
+  for model in bert-large-uncased-whole-word-masking \
+               xlnet-large-cased \
+               roberta-base \
+               xlm-mlm-en-2048 \
+               gpt2-xl \
+               albert-xxlarge-v2 \
+               ctrl ; do
+
+            model_list[$i]="${model}"
+            benchmark_list[$i]="$benchmark"
+            i=$[$i+1]
+    done
+done
+
+module add openmind/singularity
+export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
+RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching
+export RESULTCACHING_HOME
+XDG_CACHE_HOME=/om/user/`whoami`/st
+export XDG_CACHE_HOME
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running benchmark ${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
+echo "cache id " $RESULTCACHING_HOME
+
+s
+. ~/.bash_profile
+. ~/.bashrc
+conda activate neural_nlp_2022
+
+which python
+
+/om/user/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om/user/ehoseini/neural-nlp-2022/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
