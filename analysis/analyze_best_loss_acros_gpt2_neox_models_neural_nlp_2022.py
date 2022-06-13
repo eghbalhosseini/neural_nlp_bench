ee225d2b14c0e5dbb68600e6a47ec6a686506311
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-06-13 10:14:11 -0400

    running best model

---
+++ analysis/analyze_best_loss_acros_gpt2_neox_models_neural_nlp_2022.py
@@ -0,0 +1,75 @@
+import pickle as pkl
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+#%%
+import getpass
+import sys
+import pickle
+import xarray as xr
+from glob import glob
+user=getpass.getuser()
+print(user)
+import re
+from tqdm import tqdm
+
+if user=='eghbalhosseini':
+    analysis_dir='/om/user/ehoseini/MyData/NeuroBioLang_2022//analysis/'
+    result_dir='/om/user/ehoseini/MyData/NeuroBioLang_2022/'
+elif user=='ehoseini':
+    analysis_dir='/om/user/ehoseini/MyData/NeuroBioLang_2022//analysis/'
+    result_dir='/om/user/ehoseini/MyData/NeuroBioLang_2022/'
+    result_caching='/om5/group/evlab/u/ehoseini/.result_caching/'
+
+if __name__ == "__main__":
+    benchmark='Pereira2018-encoding'
+    model_1B='gpt2-neox-pos_learned-1B'
+    loss_1B_ckpnt='155000'
+    model_100M = 'gpt2-neox-pos_learned-100M'
+    loss_100M_ckpnt='11600'
+    model_10M = 'gpt2-neox-pos_learned-10M'
+    loss_10m_ckpnt='1900'
+    files=glob(os.path.join(result_caching,'neural_nlp.score',f'benchmark={benchmark},model={model}*.pkl'))
+    # order files
+    chkpoints=[re.findall(r'ckpnt-\d+',x)[0] for x in files]
+    chkpoints=[int(x.replace('ckpnt-','')) for x in chkpoints]
+    reorder=np.argsort(chkpoints)
+    chkpoints_srt=[chkpoints[x] for x in reorder]
+    files_srt=[files[x] for x in reorder]
+
+    scores_mean=[]
+    scors_std=[]
+    for ix, file in tqdm(enumerate(files_srt)):
+        x=pd.read_pickle(file)['data'].values
+        scores_mean.append(x[:,0])
+        scors_std.append(x[:,1])
+    l_names=pd.read_pickle(file)['data'].layer.values
+    cmap_all = cm.get_cmap('plasma')
+    all_col = cmap_all(np.divide(np.arange(len(scores_mean)), len(scores_mean)))
+    fig = plt.figure(figsize=(11, 8), dpi=250, frameon=False)
+    ax = plt.axes((.1, .2, .45, .35))
+    for idx,scr in enumerate(scores_mean):
+        r3 = np.arange(len(scr))
+        ax.plot(r3, scr, color=all_col[idx,:],linewidth=2,label=f'ck:{chkpoints_srt[idx]}')
+        ax.errorbar(r3, scr, yerr=scors_std[idx], linewidth=2, color=all_col[idx, :],marker='.', markersize=10)
+    ax.axhline(y=0, color='k', linestyle='-')
+    ax.legend(bbox_to_anchor=(1.4, 2), frameon=True,fontsize=8)
+    ax.set_xlim((0-.5,len(l_names)-.5))
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.set_xticks(np.arange(len(scr)))
+
+    ax.set_xticklabels(l_names, rotation=90, fontsize=12)
+    ax.set_ylabel('Pearson Corr')
+    ax = plt.axes((.1, .6, .45, .35))
+    ax.imshow(np.stack(scores_mean),vmax=1,aspect='auto')
+    ax.set_title(f'model:{model}, benchmark {benchmark}')
+    fig.show()
+
+    fig.savefig(os.path.join(analysis_dir,f'chpnt_score_{model}_{benchmark}.png'), dpi=250, format='png', metadata=None,
+        bbox_inches=None, pad_inches=0.1,facecolor='auto', edgecolor='auto',backend=None)
+
+    fig.savefig(os.path.join(analysis_dir, f'chpnt_score_{model}_{benchmark}.eps'), format='eps',metadata=None,
+                bbox_inches=None, pad_inches=0.1,facecolor='auto', edgecolor='auto',backend=None)
---
+++ om_run_gpt_neox_10M_100M_1B_best_loss_on_neural_nlp_2022.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=10M
+#SBATCH --array=0-5
+#SBATCH --time=6-23:00:00
+#SBATCH --mem=80G
+#SBATCH --exclude node017,node018
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ehoseini@mit.edu
+
+i=0
+for benchmark in Fedorenko2016v3-encoding Pereira2018-encoding ; do
+  for model in gpt2-neox-pos_learned-10M-v2-ckpnt-1900 \
+               gpt2-neox-pos_learned-100M-v2-ckpnt-11600 \
+               gpt2-neox-pos_learned-100M-v2-ckpnt-155000 ; do
+            model_list[$i]="${model}"
+            benchmark_list[$i]="$benchmark"
+            i=$[$i+1]
+      done
+    done
+done
+
+module add openmind/singularity
+export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
+RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching
+export RESULTCACHING_HOME
+XDG_CACHE_HOME=/om/user/`whoami`/st
+export XDG_CACHE_HOME
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running benchmark ${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
+
+. ~/.bash_profile
+. ~/.bashrc
+conda activate neural_nlp_2022
+
+/om/user/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om/user/ehoseini/neural-nlp-2022/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
