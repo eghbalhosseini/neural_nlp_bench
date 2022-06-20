64c60b56d021af911685c10103b41f8b415a4520
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-06-20 09:40:52 -0400

    more checkpoints

---
+++ convert_gpt_neox_ckpnt.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH --exclude node[017-018]
+#SBATCH -t 5:00:00
+
+GRAND_FILE=$1
+OVERWRITE='false' # or 'true'
+#
+
+if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+  JID=$SLURM_ARRAY_TASK_ID    # Taking the task ID in a job array as an input parameter.
+else
+  JID=$2       # Taking the task ID as an input parameter.
+fi
+echo "${GRAND_FILE}"
+echo $JID
+
+while IFS=, read -r line_count ckpoint_name ckpont_dir ; do
+  #echo "line_count ${model}"
+  if [ $JID == $line_count ]
+    then
+      echo "found the right match ${line_count}"
+      run_ckpoint_name=$ckpoint_name
+      run_ckpont_dir=$ckpont_dir
+      do_run=true
+      break
+    else
+      do_run=false
+  fi
+done <"${GRAND_FILE}"
+if $do_run; then
+  echo "run_ckpoint_name:${run_ckpoint_name}"
+  echo "run_ckpont_dir:${run_ckpont_dir}"
+
+  . ~/.bash_profile
+  conda activate neural_nlp_2022
+  echo $(which python)
+  python /om/user/ehoseini/neural-nlp-2022/neural_nlp/models/gpt_neox_model/convert_ckpt_to_gpt_neox.py --checkpoint_dir "$run_ckpont_dir" --hf_save_dir "$run_ckpont_dir"
+
+fi
---
+++ run_convert_gpt_neox_ckpnt.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+ROOT_DIR='/om/user/ehoseini/MyData/miniBERTa_training/miniBERTa_1b_v2/gpt2/checkpoints_4/'
+
+ckpnt_conv_='ckpnt_conv_list'
+i=0
+LINE_COUNT=0
+CKPNT_FILE="${ROOT_DIR}/${ckpnt_conv_}.txt"
+rm -f $CKPNT_FILE
+touch $CKPNT_FILE
+printf "%s,%s,%s\n" "row" "ckpnt_name" "location"   >> $CKPNT_FILE
+
+echo "looking at ${ROOT_DIR} "
+SUBJ_LINE=0
+while read x; do
+      # check if file already exist in labels dir
+      original=$ROOT_DIR
+      correction=''
+      ckpont_name="${x/$original/$correction}"
+
+
+      LINE_COUNT=$(expr ${LINE_COUNT} + 1)
+        # folder to find the file
+
+      printf "%d,%s,%s\n" "$LINE_COUNT" "$ckpont_name" "$x" >> $CKPNT_FILE
+done < <(find $ROOT_DIR -maxdepth 1 -type d -name "global*")
+
+run_val=0
+if [ "$LINE_COUNT" -gt "$run_val" ]; then
+  echo "running  ${LINE_COUNT} jobs"
+
+   #nohup /cm/shared/admin/bin/submit-many-jobs 3 2 3 1 convert_gpt_neox_ckpoint.sh  $CKPNT_FILE
+   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 75 100 25 convert_gpt_neox_ckpoint.sh  $CKPNT_FILE &
+  else
+    echo $LINE_COUNT
+fi
