a258a3b629d91a46661297e79cde3a94cda6ae43
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-07-11 17:37:34 -0400

    changing partition

--- om_run_mistral_untrained_neural_nlp_2022.sh
+++ om_run_mistral_untrained_neural_nlp_2022.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=MISTRL
 #SBATCH --array=0-2
 #SBATCH --time=6-23:00:00
-#SBATCH --mem=40G
+#SBATCH --mem=60G
 #SBATCH --partition evlab
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
@@ -31,28 +31,28 @@ echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running benchmark ${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
 
 
-if [ $overwrite ]
-then
-  x=${benchmark_list[$SLURM_ARRAY_TASK_ID]}
-  original='-encoding'
-  correction=''
-  activity_name="${x/$original/$correction}"
-  ACT_DIR="${RESULTCACHING_HOME}/neural_nlp.models.wrapper.core.ActivationsExtractorHelper._from_sentences_stored/"
-  act_name="identifier=${model_list[$SLURM_ARRAY_TASK_ID]},*"
-
-  find $ACT_DIR -type f -iname $act_name -printf x | wc -c
-
-  find $ACT_DIR -type f -iname $act_name -exec rm -rf {} \;
-
-  SCORE_DIR="${RESULTCACHING_HOME}/neural_nlp.score/"
-  score_name="benchmark=${benchmark_list[$SLURM_ARRAY_TASK_ID]},model=${model_list[$SLURM_ARRAY_TASK_ID]}*"
-
-  find $ACT_DIR -type f -iname $act_name -printf x | wc -c
-
-  find $SCORE_DIR -type f -iname $score_name -exec rm -rf {} \;
-
-  echo " removed prior data "
-fi
+#if [ $overwrite ]
+#then
+#  x=${benchmark_list[$SLURM_ARRAY_TASK_ID]}
+#  original='-encoding'
+#  correction=''
+#  activity_name="${x/$original/$correction}"
+#  ACT_DIR="${RESULTCACHING_HOME}/neural_nlp.models.wrapper.core.ActivationsExtractorHelper._from_sentences_stored/"
+#  act_name="identifier=${model_list[$SLURM_ARRAY_TASK_ID]},*"
+#
+#  find $ACT_DIR -type f -iname $act_name -printf x | wc -c
+#
+#  find $ACT_DIR -type f -iname $act_name -exec rm -rf {} \;
+#
+#  SCORE_DIR="${RESULTCACHING_HOME}/neural_nlp.score/"
+#  score_name="benchmark=${benchmark_list[$SLURM_ARRAY_TASK_ID]},model=${model_list[$SLURM_ARRAY_TASK_ID]}*"
+#
+#  find $ACT_DIR -type f -iname $act_name -printf x | wc -c
+#
+#  find $SCORE_DIR -type f -iname $score_name -exec rm -rf {} \;
+#
+#  echo " removed prior data "
+#fi
 
 
 #. ~/.bash_profile
