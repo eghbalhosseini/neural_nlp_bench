7441201d093b1a9a995c1afd394068006ebe7904
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-07-15 10:00:10 -0400

    adding blacn and futrell

--- om_run_mistral_chkpnt_on_neural_nlp_2022.sh
+++ om_run_mistral_chkpnt_on_neural_nlp_2022.sh
@@ -2,15 +2,15 @@
 #SBATCH --job-name=MISTRAL
 #SBATCH --array=0-40
 #SBATCH --time=6-23:00:00
-#SBATCH --mem=80G
+#SBATCH --mem=40G
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for benchmark in Pereira2018-encoding ; do
+for benchmark in Blank2014fROI-encoding Futrell2018-encoding ; do
   for model in mistral/caprica-gpt2-small-x81  ; do
-      for checkpoint in `seq 0 10000 400000`; do
+      for checkpoint in `seq 0 20000 400000`; do
             model_list[$i]="${model}/ckpt_${checkpoint}"
             benchmark_list[$i]="$benchmark"
             i=$[$i+1]
@@ -18,12 +18,10 @@ for benchmark in Pereira2018-encoding ; do
     done
 done
 
-module add openmind/singularity
-export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
+
+
 RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching
 export RESULTCACHING_HOME
-XDG_CACHE_HOME=/om/user/`whoami`/st
-export XDG_CACHE_HOME
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running benchmark ${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
