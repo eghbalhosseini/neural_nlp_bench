3a7d2a8e12625770d08c477e4fdcced1a9186561
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-07-17 21:00:35 -0400

    bugfix

--- om_run_mistral_chkpnt_40_400_4K_40K_400K_on_neural_nlp_2022.sh
+++ om_run_mistral_chkpnt_40_400_4K_40K_400K_on_neural_nlp_2022.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=MISTRAL
-#SBATCH --array=0-4
+#SBATCH --array=0-15
 #SBATCH --time=6-23:00:00
 #SBATCH --mem=40G
 #SBATCH --exclude node017,node018
@@ -8,7 +8,7 @@
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for benchmark in Fedorenko2016v3-encoding ; do
+for benchmark in Pereira2018-encoding Blank2014fROI-encoding Futrell2018-encoding  ; do
   for model in mistral/caprica-gpt2-small-x81  ; do
       for checkpoint in 40 400 4000 40000 400000; do
             model_list[$i]="${model}/ckpt_${checkpoint}"
