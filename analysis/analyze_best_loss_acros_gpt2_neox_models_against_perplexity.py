0630d9bf94c3eeff445c09b6a4caf0762aa1b71d
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-07-19 17:35:17 -0400

    trying blank

--- analysis/analyze_best_loss_acros_gpt2_neox_models_against_perplexity.py
+++ analysis/analyze_best_loss_acros_gpt2_neox_models_against_perplexity.py
@@ -24,9 +24,10 @@ elif user=='ehoseini':
     result_caching='/om5/group/evlab/u/ehoseini/.result_caching/'
 
 if __name__ == "__main__":
-    #benchmark='Pereira2018-encoding'
-    benchmark = 'Blank2014fROI-encoding'
+    benchmark='Pereira2018-encoding'
+    #benchmark = 'Blank2014fROI-encoding'
     #benchmark = 'Futrell2018-encoding'
+    #benchmark = 'Fedorenko2016v3-encoding'
     model_1B='gpt2-neox-pos_learned-1B'
     loss_1B_ckpnt='310000'
     model_100M = 'gpt2-neox-pos_learned-100M'
@@ -51,6 +52,8 @@ if __name__ == "__main__":
         scors_std.append(x[:,1])
     # get perplexity results:
     score_max=[max(x) for x in scores_mean]
+    score_loc=[np.argmax(x) for x in scores_mean]
+    score_std=[scors_std[x][score_loc[x]] for x in range(len(score_loc))]
     training_results = pd.read_pickle(os.path.join(result_dir, 'data', 'miniberta_train_valid_set.pkl'))
     # find the location of perplexity score closest to checkpoint:
     training_chkpnt = [training_results[key]['validation_result'][:, 0].astype(int) for key in training_results.keys()]
@@ -76,6 +79,7 @@ if __name__ == "__main__":
     ax.plot(validation_perpelxity, validation_score, zorder=1, color=(.5,.5,.5))
     for idx in range(len(validation_score)):
         ax.scatter(validation_perpelxity[idx], validation_score[idx],s=50, c=(all_col[idx,:]), zorder=2,label=chkpoints_srt[idx])
+        ax.errorbar(validation_perpelxity[idx], validation_score[idx], yerr=score_std[idx], linewidth=2, color=all_col[idx, :], marker='.', markersize=10)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.set_ylabel('Pearson Corr')
@@ -87,4 +91,31 @@ if __name__ == "__main__":
         bbox_inches=None, pad_inches=0.1,facecolor='auto', edgecolor='auto',backend=None)
 
     fig.savefig(os.path.join(analysis_dir, f'chpnt_score_best_loss_gpt_neox_{benchmark}_against_perplexity.eps'), format='eps',metadata=None,
+                bbox_inches=None, pad_inches=0.1,facecolor='auto', edgecolor='auto',backend=None)
+
+
+    #%% do the same analysis but take the 1b model and find how it behaves
+
+    score_max_1b = np.argmax(scores_mean[-1])
+    scores_max= [scores_mean[x][score_max_1b] for x in range(len(scores_mean))]
+    score_std = [scors_std[x][score_max_1b] for x in range(len(scores_mean))]
+    validation_score = np.asarray(scores_max)
+
+    fig = plt.figure(figsize=(11, 8), dpi=250, frameon=False)
+    ax = plt.axes((.1, .2, .45, .35))
+    ax.plot(validation_perpelxity, validation_score, zorder=1, color=(.5,.5,.5))
+    for idx in range(len(validation_score)):
+        ax.scatter(validation_perpelxity[idx], validation_score[idx],s=50, c=(all_col[idx,:]), zorder=2,label=chkpoints_srt[idx])
+        ax.errorbar(validation_perpelxity[idx], validation_score[idx], yerr=score_std[idx], linewidth=2, color=all_col[idx, :], marker='.', markersize=10)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.set_ylabel('Pearson Corr')
+    ax.set_xlabel('perplexity')
+    ax.legend(bbox_to_anchor=(1.2, .8), frameon=True, fontsize=8)
+    ax.set_title(f'model:gpt_neox \n benchmark {benchmark} against perplexity \n for best layer in 1B ')
+    fig.show()
+    fig.savefig(os.path.join(analysis_dir,f'chpnt_score_best_loss_gpt_neox_{benchmark}_against_perplexity_for_best_layer_in_1b.png'), dpi=250, format='png', metadata=None,
+        bbox_inches=None, pad_inches=0.1,facecolor='auto', edgecolor='auto',backend=None)
+
+    fig.savefig(os.path.join(analysis_dir, f'chpnt_score_best_loss_gpt_neox_{benchmark}_against_perplexity_in_1b.eps'), format='eps',metadata=None,
                 bbox_inches=None, pad_inches=0.1,facecolor='auto', edgecolor='auto',backend=None)
