c77c5744312387307f08051571d068882b111018
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-17 20:19:46 -0400

    running permuted models

--- om_run_NYU_roberta.sh
+++ om_run_NYU_roberta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=NYU_roberta
-#SBATCH --array=0
+#SBATCH --array=0-5
 #SBATCH --time=56:00:00
 #SBATCH -c 16
 #SBATCH --mem=267G
@@ -9,8 +9,8 @@
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for benchmark in Fedorenko2016v3-encoding ; do
-for model in roberta-base ; do
+for benchmark in Pereira2018-encoding Blank2014fROI-encoding Futrell2018-encoding Futrell2018-stories_encoding Futrell2018-sentences_encoding ; do
+for model in roberta-base-untrained ; do
   model_list[$i]="$model"
   benchmark_list[$i]="$benchmark"
   i=$i+1
@@ -27,4 +27,11 @@ echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running benchmark ${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
 
-singularity exec -B /om:/om /om/user/`whoami`/simg_images/arch_search.simg python ~/neural-nlp/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}" --log_level DEBUG
+
+
+. ~/.bash_profile
+. ~/.bashrc
+conda activate neural_nlp_2022
+/om/user/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om/user/ehoseini/neural-nlp-2022/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
+
+#singularity exec -B /om:/om /om/user/`whoami`/simg_images/arch_search.simg python ~/neural-nlp/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}" --log_level DEBUG
