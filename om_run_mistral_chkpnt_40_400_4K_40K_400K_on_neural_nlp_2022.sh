dc2957a6a068a2fee1a6dc344093735b5d6bf45f
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-26 14:16:39 -0400

    running permuted models

--- om_run_mistral_chkpnt_40_400_4K_40K_400K_on_neural_nlp_2022.sh
+++ om_run_mistral_chkpnt_40_400_4K_40K_400K_on_neural_nlp_2022.sh
@@ -9,22 +9,24 @@
 
 
 i=0
-overwrite=false
+overwrite=true
 
 #activity_id_list="Fedorenko2016.ecog"
-activity_id_list="naturalStories naturalStories"
+activity_id_list="naturalStories naturalStories naturalStories"
 activity_arr=($activity_id_list)
 
-for benchmark in Futrell2018-encoding Futrell2018-stories_encoding Futrell2018-sentences_encoding ; do
+for benchmark in Futrell2018-encoding ; do
 #for benchmark in Futrell2018-encoding Futrell2018-stories_encoding Futrell2018-sentences_encoding ; do
-  for model in alias-gpt2-small-x21 expanse-gpt2-small-x777  ; do
-      for checkpoint in 0 40 400 4000 40000 400000; do
-      #for checkpoint in 4000 ; do
-            model_list[$i]="${model}-ckpnt-${checkpoint}"
+  for model in alias-gpt2-small-x21-ckpnt-400 expanse-gpt2-small-x777-ckpnt-400 \
+   alias-gpt2-small-x21-ckpnt-4000 alias-gpt2-small-x21-ckpnt-40000 alias-gpt2-small-x21-ckpnt-400000 ; do
+      #for checkpoint in 400 4000 40000 400000; do
+
+            #model_list[$i]="${model}-ckpnt-${checkpoint}"
+            model_list[$i]="${model}"
             benchmark_list[$i]="$benchmark"
             activity_list[$i]="${activity_arr[$idx]}"
             i=$[$i+1]
-      done
+      #done
     done
 done
 
@@ -39,32 +41,32 @@ echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running benchmark ${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
 
-#if [ $overwrite ]
-#then
-#  #original='mistral/'
-#  #correction='mistral_'
-#  #model_fix="${x/$original/$correction}"
-#
-#  #x=$model_fix
-#  #original='/ckpt'
-#  #correction='_ckpt'
-#  #model_name="${x/$original/$correction}"
-#
-#  activity_name=${activity_list[$SLURM_ARRAY_TASK_ID]}
-#  model_name=${model_list[$SLURM_ARRAY_TASK_ID]}
-#  ACT_DIR="${RESULTCACHING_HOME}/neural_nlp.models.wrapper.core.ActivationsExtractorHelper._from_sentences_stored/"
-#  act_name="identifier=${model_name},stimuli_identifier=${activity_name}*"
-#  echo "searching for ${act_name}"
-#  find $ACT_DIR -type f -iname $act_name -printf x | wc -c
-#  find $ACT_DIR -type f -iname $act_name -exec rm -rf {} \;
-#
-#  SCORE_DIR="${RESULTCACHING_HOME}/neural_nlp.score/"
-#  score_name="benchmark=${benchmark_list[$SLURM_ARRAY_TASK_ID]},model=${model_list[$SLURM_ARRAY_TASK_ID]},*"
-#  echo "searching for ${score_name}"
-#  find $ACT_DIR -type f -iname $act_name -printf x | wc -c
-#  find $SCORE_DIR -type f -iname $score_name -exec rm -rf {} \;
-#  echo " removed prior data "
-#fi
+if [ "$overwrite" = true ]
+then
+  #original='mistral/'
+  #correction='mistral_'
+  #model_fix="${x/$original/$correction}"
+
+  #x=$model_fix
+  #original='/ckpt'
+  #correction='_ckpt'
+  #model_name="${x/$original/$correction}"
+
+  activity_name=${activity_list[$SLURM_ARRAY_TASK_ID]}
+  model_name=${model_list[$SLURM_ARRAY_TASK_ID]}
+  ACT_DIR="${RESULTCACHING_HOME}/neural_nlp.models.wrapper.core.ActivationsExtractorHelper._from_sentences_stored/"
+  act_name="identifier=${model_name},stimuli_identifier=${activity_name}*"
+  echo "searching for ${act_name}"
+  find $ACT_DIR -type f -iname $act_name -printf x | wc -c
+  find $ACT_DIR -type f -iname $act_name -exec rm -rf {} \;
+
+  SCORE_DIR="${RESULTCACHING_HOME}/neural_nlp.score/"
+  score_name="benchmark=${benchmark_list[$SLURM_ARRAY_TASK_ID]},model=${model_list[$SLURM_ARRAY_TASK_ID]},*"
+  echo "searching for ${score_name}"
+  find $ACT_DIR -type f -iname $act_name -printf x | wc -c
+  find $SCORE_DIR -type f -iname $score_name -exec rm -rf {} \;
+  echo " removed prior data "
+fi
 
 . ~/.bash_profile
 . ~/.bashrc
