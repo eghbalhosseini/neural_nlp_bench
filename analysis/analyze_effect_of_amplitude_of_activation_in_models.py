4d2ac15144ea016294b6983075493aeaf8a9caf5
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-09-26 12:04:15 -0400

    running permuted models

--- analysis/analyze_effect_of_amplitude_of_activation_in_models.py
+++ analysis/analyze_effect_of_amplitude_of_activation_in_models.py
@@ -26,43 +26,62 @@ elif user=='ehoseini':
 
 if __name__ == "__main__":
     stimuli_name='Pereira2018-*'
-    model='mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-5'
-    print(model)
-    activtiy_folder='neural_nlp.models.wrapper.core.ActivationsExtractorHelper._from_sentences_stored'
-    files=glob(os.path.join(result_caching,activtiy_folder,f'identifier={model},stimuli_identifier={stimuli_name}*.pkl'))
-    # order files
+    model_list=['mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-1',
+                'mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-2',
+                'mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-3',
+                'mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-4',
+                'mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-5',
+                'mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-6',
+                'mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-7',
+                'mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-std-2',
+                'mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-std-1',
+                'mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-mu-2',
+                'mistral-caprica-gpt2-small-x81-ckpnt-400000-untrained-mu-1',
+                'bplm-gpt2-hf-init-ckpnt-39000',
+                'bplm-gpt2-hf-init-ckpnt-0',
+                'bplm-gpt2-gauss-init-1-ckpnt-44500',
+                'bplm-gpt2-gauss-init-1-ckpnt-0',
+                'mistral_caprica-gpt2-small-x81-ckpnt-0',
+                'mistral-caprica-gpt2-small-x81-ckpnt-0-untrained'
+                ]
+    for model in model_list:
 
-    files_srt=files
-    # remove chekcpoint zero if exist
-    norms=[]
-    for ix, file in tqdm(enumerate(files_srt)):
-        x=pd.read_pickle(file)['data']
-        layer_norm=[]
-        for grp in x.groupby('layer'):
-            layer_norm.append(linalg.norm(grp[1], axis=1))
-        norms.append(np.stack(layer_norm))
+        print(model)
+        activtiy_folder='neural_nlp.models.wrapper.core.ActivationsExtractorHelper._from_sentences_stored'
+        files=glob(os.path.join(result_caching,activtiy_folder,f'identifier={model},stimuli_identifier={stimuli_name}*.pkl'))
+        # order files
 
-    norms=np.concatenate(norms,axis=1)
+        files_srt=files
+        # remove chekcpoint zero if exist
+        norms=[]
+        for ix, file in tqdm(enumerate(files_srt)):
+            x=pd.read_pickle(file)['data']
+            layer_norm=[]
+            for grp in x.groupby('layer'):
+                layer_norm.append(linalg.norm(grp[1], axis=1))
+            norms.append(np.stack(layer_norm))
 
-    cmap_all = cm.get_cmap('plasma')
+        norms=np.concatenate(norms,axis=1)
 
-    all_col = cmap_all(np.divide(np.arange(len(norms)), len(norms)))
-    fig = plt.figure(figsize=(11, 8), dpi=250, frameon=False)
-    ax = plt.axes((.1, .2, .45, .35))
-    idx=0
-    r3 = np.arange(norms.shape[0])
-    ax.plot(r3, np.mean(norms,axis=1), color=all_col[idx,:],linewidth=2)
-    ax.errorbar(r3, np.mean(norms,axis=1), yerr=np.std(norms,axis=1), linewidth=2, color=all_col[idx, :],marker='.', markersize=10)
-    ax.axhline(y=0, color='k', linestyle='-')
-    ax.legend(bbox_to_anchor=(1.4, 2), frameon=True,fontsize=8)
-    ax.set_xlim((0-.5,norms.shape[0]-.5))
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.set_xticks(np.arange(norms.shape[0]))
-    #fig.show()
+        cmap_all = cm.get_cmap('plasma')
 
-    fig.savefig(os.path.join(analysis_dir,f'activations_{model}_.png'), dpi=250, format='png', metadata=None,
-        bbox_inches=None, pad_inches=0.1,facecolor='auto', edgecolor='auto',backend=None)
+        all_col = cmap_all(np.divide(np.arange(len(norms)), len(norms)))
+        fig = plt.figure(figsize=(11, 8), dpi=250, frameon=False)
+        ax = plt.axes((.1, .2, .45, .35))
+        idx=0
+        r3 = np.arange(norms.shape[0])
+        ax.plot(r3, np.mean(norms,axis=1), color=all_col[idx,:],linewidth=2)
+        ax.errorbar(r3, np.mean(norms,axis=1), yerr=np.std(norms,axis=1), linewidth=2, color=all_col[idx, :],marker='.', markersize=10)
+        ax.axhline(y=0, color='k', linestyle='-')
+        ax.legend(bbox_to_anchor=(1.4, 2), frameon=True,fontsize=8)
+        ax.set_xlim((0-.5,norms.shape[0]-.5))
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.set_xticks(np.arange(norms.shape[0]))
+        #fig.show()
+
+        fig.savefig(os.path.join(analysis_dir,f'activations_{model}_.png'), dpi=250, format='png', metadata=None,
+            bbox_inches=None, pad_inches=0.1,facecolor='auto', edgecolor='auto',backend=None)
 
     #fig.savefig(os.path.join(analysis_dir, f'effect_of_mu_{model}_{benchmark}.eps'), format='eps',metadata=None,
     #            bbox_inches=None, pad_inches=0.1,facecolor='auto', edgecolor='auto',backend=None)
