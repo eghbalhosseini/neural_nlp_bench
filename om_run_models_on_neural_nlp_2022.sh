c173df1e475de31756fb3b1fe15752c993dd0f3d
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-07-09 17:10:43 -0400

    running benchmarks for average

--- om_run_models_on_neural_nlp_2022.sh
+++ om_run_models_on_neural_nlp_2022.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=nlp2022
-#SBATCH --array=1-22
-#SBATCH --time=32:00:00
+#SBATCH --array=1-2
+#SBATCH --time=12:00:00
 #SBATCH --mem=20G
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=1
-for benchmark in  Pereira2023aud-sent-passage-RidgeEncoding \
-                  Pereira2023aud-sent-sentence-RidgeEncoding; do
-  for model in  roberta-base \
-                xlnet-large-cased \
-                bert-large-uncased-whole-word-masking \
-                xlm-mlm-en-2048 \
-                gpt2-xl \
-                albert-xxlarge-v2 \
-                ctrl \
-                distilgpt2 \
-                gpt2 \
-                gpt2-medium \
-                gpt2-large ; do
+for benchmark in Pereira2018-norm-encoding ; do
+  for model in  gpt2 \
+                gpt2-untrained  ; do
             model_list[$i]="${model}"
             benchmark_list[$i]="$benchmark"
             i=$[$i+1]
