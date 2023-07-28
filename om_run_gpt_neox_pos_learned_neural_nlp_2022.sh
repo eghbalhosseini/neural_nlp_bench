3b70907e7b285192f6e11ad5282c1085659fd333
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-07-28 14:08:55 -0400

    running 50M

--- om_run_gpt_neox_pos_learned_neural_nlp_2022.sh
+++ om_run_gpt_neox_pos_learned_neural_nlp_2022.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=nlp2022
-#SBATCH --array=0-102
+#SBATCH --array=0-2
 #SBATCH --time=12:00:00
 #SBATCH -c 16
 #SBATCH --mem=60G
@@ -9,9 +9,10 @@
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for benchmark in  Pereira2018-encoding Pereira2018-norm-encoding Pereira2018-norm-v2-encoding  Futrell2018-encoding Futrell2018-norm-v2-encoding Futrell2018-norm-encoding ; do
-  for model in  'gpt2' ; do
-    for trained in '' 'untrained' 'untrained_hf' 'permuted' 'untrained-1' 'untrained-2' 'untrained-3' 'untrained-4' 'untrained-5' 'untrained-6' 'untrained-7' 'untrained-std-1' 'untrained-std-2' 'untrained-mu-1' 'untrained-mu-2' 'untrained-ln-hf' 'untrained-ln-uniform' ; do
+for benchmark in  Pereira2018-encoding  Futrell2018-encoding  ; do
+  for model in  gpt2-neox-pos_learned-50M-v3-ckpnt-8500 ; do
+    for trained in '' ; do
+    # 'untrained' 'untrained_hf' 'permuted' 'untrained-1' 'untrained-2' 'untrained-3' 'untrained-4' 'untrained-5' 'untrained-6' 'untrained-7' 'untrained-std-1' 'untrained-std-2' 'untrained-mu-1' 'untrained-mu-2' 'untrained-ln-hf' 'untrained-ln-uniform'
     #gpt2-neox-pos_learned-1B-v3-ckpnt-310000-untrained-ln-uniform \
                 #gpt2-neox-pos_learned-10M-v3-ckpnt-2000 gpt2-neox-pos_learned-10M-v3-ckpnt-2000-untrained gpt2-neox-pos_learned-10M-v3-ckpnt-2000-untrained_hf \
                 #gpt2-neox-pos_learned-100M-v3-ckpnt-14250 gpt2-neox-pos_learned-100M-v3-ckpnt-14250-untrained gpt2-neox-pos_learned-100M-v3-ckpnt-14250-untrained_hf \
@@ -36,7 +37,6 @@ echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
 echo "Running benchmark ${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
 
 . ~/.bash_profile
-. ~/.bashrc
 conda activate neural_nlp_2022
 
 /om/weka/evlab/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om/weka/evlab/ehoseini/neural-nlp-2022/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
