c2f5256e483615796eecbd048a6ea1503da12c7f
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-08-07 12:30:09 -0400

    new dsparametric

--- om_run_mistral_chkpnt_on_neural_nlp_2022.sh
+++ om_run_mistral_chkpnt_on_neural_nlp_2022.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 #SBATCH --job-name=MISTRAL
-#SBATCH --array=1-35
-#SBATCH --time=40:00:00
+#SBATCH --array=1-12
+#SBATCH --time=12:00:00
 #SBATCH --mem=40G
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
 i=1
-#for benchmark in  Pereira2018-v2-encoding Pereira2018-norm-encoding ; do
-#  for model in mistral-caprica-gpt2-small-x81  ; do
-#      for checkpoint in 200000 ; do
-#        for train in '' '-untrained' '-untrained_hf' '-permuted' '-untrained-1' '-untrained-2' '-untrained-3' '-untrained-4' '-untrained-5' '-untrained-6' '-untrained-7' '-untrained-std-1' \
-#           '-untrained-std-2' '-untrained-mu-1' '-untrained-mu-2' '-untrained-ln-hf' '-untrained-ln-uniform' ; do
-#            model_list[$i]="${model}-ckpnt-${checkpoint}${train}"
-#            benchmark_list[$i]="$benchmark"
-#            i=$[$i+1]
-#          done
-#      done
-#    done
-#done
-
-
-for benchmark in  Pereira2018-v2-encoding Pereira2018-norm-encoding ; do
-  for model in gpt2  ; do
-
-        for train in '' '-untrained' '-untrained_hf' '-permuted' '-untrained-1' '-untrained-2' '-untrained-3' '-untrained-4' '-untrained-5' '-untrained-6' '-untrained-7' '-untrained-std-1' \
-           '-untrained-std-2' '-untrained-mu-1' '-untrained-mu-2' '-untrained-ln-hf' '-untrained-ln-uniform' ; do
-            model_list[$i]="${model}${train}"
+for benchmark in  Pereira2018-v2-encoding ; do
+  for model in expanse-gpt2-small-x777 alias-gpt2-small-x21  ; do
+      for checkpoint in 0 20 200 2000 20000 200000 ; do
+        for train in '' ; do
+            model_list[$i]="${model}-ckpnt-${checkpoint}${train}"
             benchmark_list[$i]="$benchmark"
             i=$[$i+1]
+          done
       done
     done
 done
+
+
+#for benchmark in  Pereira2018-v2-encoding ; do
+#  for model in gpt2  ; do
+#
+#        for train in '' '-untrained' '-untrained_hf' '-permuted' '-untrained-1' '-untrained-2' '-untrained-3' '-untrained-4' '-untrained-5' '-untrained-6' '-untrained-7' '-untrained-std-1' \
+#           '-untrained-std-2' '-untrained-mu-1' '-untrained-mu-2' '-untrained-ln-hf' '-untrained-ln-uniform' ; do
+#            model_list[$i]="${model}${train}"
+#            benchmark_list[$i]="$benchmark"
+#            i=$[$i+1]
+#      done
+#    done
+#done
 
 
 
