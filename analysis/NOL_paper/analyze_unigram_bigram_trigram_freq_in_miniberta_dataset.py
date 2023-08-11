680ba667e41c692b608a1ef064841bd194b1a77b
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-08-11 18:19:22 -0400

    bugfix

--- analysis/NOL_paper/analyze_unigram_bigram_trigram_freq_in_miniberta_dataset.py
+++ analysis/NOL_paper/analyze_unigram_bigram_trigram_freq_in_miniberta_dataset.py
@@ -17,26 +17,33 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--data', type=str, default='10M')
 parser.add_argument('--ngram', type=int, default=2)
+parser.add_argument('--chunk_id', type=int, default=0)
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
     data=args.data
     ngram=int(args.ngram)
+    chunk_id=int(args.chunk_id)
     #data = '10M'
     # check if pickle file exists
-    if os.path.exists(f'/om2/user/ehoseini/MyData/miniBERTa_v2/miniBERTa-{data}/counts_dictionary_{ngram}.pkl'):
+    if os.path.exists(f'/om2/user/ehoseini/MyData/miniBERTa_v2/miniBERTa-{data}/counts_dictionary_{ngram}_chunk_{chunk_id}.pkl'):
         print('pickle file exists')
         exit()
     else:
         print('pickle file does not exist')
     mini_dataset = load_dataset('/om2/user/ehoseini/MyData/miniBERTa_v2', f'miniBERTa-{data}')
+    # create a np.arange of indices from 0 to len(mini_dataset['train'])
+    indices=np.arange(len(mini_dataset['train']))
+    # split it to 50 chuncks
+    indices_split=np.array_split(indices,50)
+    mini_dataset_sample = mini_dataset['train'].select(indices_split[chunk_id])
     counts_dictionary={}
         # print what ngram is recorded
     print('ngram: ', ngram)
 
     counts=Counter()
-    for text in tqdm(mini_dataset['train'],total=len(mini_dataset['train'])):
+    for text in tqdm(mini_dataset_sample,total=len(mini_dataset_sample)):
         counts.update(Counter(ngrams(nltk.word_tokenize(text['text']), n=ngram)))
     counts_dictionary[ngram]=counts
 
@@ -45,6 +52,6 @@ if __name__ == "__main__":
 
     # save the dictionary of counts as a pickle file
 
-    with open(f'/om2/user/ehoseini/MyData/miniBERTa_v2/miniBERTa-{data}/counts_dictionary_ngram_{ngram}.pkl', 'wb') as handle:
+    with open(f'/om2/user/ehoseini/MyData/miniBERTa_v2/miniBERTa-{data}/counts_dictionary_ngram_{ngram}_chunk_{chunk_id}.pkl', 'wb') as handle:
         pickle.dump(counts_dictionary, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
--- om_run_analyze_unigram_bigram_trigam_freq_in_miniberta.sh
+++ om_run_analyze_unigram_bigram_trigam_freq_in_miniberta.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #SBATCH --job-name=nlp2022
-#SBATCH --array=0-16
+#SBATCH --array=0-199
 #SBATCH --time=24:00:00
-#SBATCH -c 16
-#SBATCH --mem=80G
+#SBATCH --mem=40G
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for data in 1M 10M 100M 1B  ; do
+for data in 1B  ; do
   for ngram in 1 2 3 4 ; do
+    # create a sequence of 50 chunk from 0 to 49
+    for chunk in $(seq 0 49); do
             data_list[$i]="${data}"
             ngram_list[$i]="${ngram}"
+            chunk_list[$i]="${chunk}"
             i=$[$i+1]
+      done
   done
 done
 
@@ -21,9 +24,7 @@ done
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 echo "Running data ${data_list[$SLURM_ARRAY_TASK_ID]}"
 
-
-
 . ~/.bash_profile
 conda activate neural_nlp_2022
 which python
-/om/weka/evlab/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om/weka/evlab/ehoseini/neural_nlp_bench/analysis/NOL_paper/analyze_unigram_bigram_trigram_freq_in_miniberta_dataset.py --data "${data_list[$SLURM_ARRAY_TASK_ID]}" --ngram "${ngram_list[$SLURM_ARRAY_TASK_ID]}"
+/om/weka/evlab/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om/weka/evlab/ehoseini/neural_nlp_bench/analysis/NOL_paper/analyze_unigram_bigram_trigram_freq_in_miniberta_dataset.py --data "${data_list[$SLURM_ARRAY_TASK_ID]}" --ngram "${ngram_list[$SLURM_ARRAY_TASK_ID]}" --chunk_id "${chunk_list[$SLURM_ARRAY_TASK_ID]}"
