1698a585adf2bc07446dfaf1b6d0c748d56822c1
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-12-28 11:13:39 -0500

    reordered the files- still needs edits

---
+++ bash/ANNSet1/om_run_langloc_and_ANN_benchmarks_on_models.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH --job-name=MGH
+#SBATCH --array=0-13
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=24G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ehoseini@mit.edu
+
+
+i=0
+
+for benchmark in LangLocECoG-bip-gamma-RidgeEncoding ANNSet1ECoG-bip-gamma-RidgeEncoding ; do
+    for model in  roberta-base \
+                  bert-large-uncased-whole-word-masking \
+                  xlnet-large-cased \
+                  xlm-mlm-en-2048 \
+                  albert-xxlarge-v2 \
+                  gpt2-xl \
+                  ctrl  ; do
+           model_list[$i]="$model"
+            benchmark_list[$i]="$benchmark"
+            i=$[$i+1]
+    done
+done
+
+
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "Running model ${model_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running benchmark ${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
+
+RESULTCACHING_HOME=/om5/group/evlab/u/ehoseini/.result_caching
+export RESULTCACHING_HOME
+
+. ~/.bash_profile
+. ~/.bashrc
+conda activate neural_nlp_2022
+
+/om2/user/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om2/user/ehoseini/neural-nlp-2022/neural_nlp run --model "${model_list[$SLURM_ARRAY_TASK_ID]}" --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}"
+
+
