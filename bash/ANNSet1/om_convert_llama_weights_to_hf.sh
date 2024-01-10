ceef32e60b91701c0d8c7b1647e3404260ce993d
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2024-01-10 16:43:05 -0500

    bugfix

--- bash/ANNSet1/om_convert_llama_weights_to_hf.sh
+++ bash/ANNSet1/om_convert_llama_weights_to_hf.sh
@@ -23,5 +23,5 @@ for model_size in 7B 13B 30B 65B ; do
 
 
 python /rdma/vast-rdma/vast/evlab/ehoseini/miniconda3/envs/neural_nlp_2022/lib/python3.9/site-packages/transformers/models/llama/convert_llama_weights_to_hf.py \
---input_dir /nese/mit/group/evlab/u/Shared/LLAMA/ --model_size "${model_list[$SLURM_ARRAY_TASK_ID]}" --output_dir /nese/mit/group/evlab/u/ehoseini/MyData/LLAMA/
+--input_dir /nese/mit/group/evlab/u/Shared/LLAMA/ --model_size "${model_list[$SLURM_ARRAY_TASK_ID]}" --output_dir "/nese/mit/group/evlab/u/ehoseini/MyData/LLAMA/${model_list[$SLURM_ARRAY_TASK_ID]}"
 
