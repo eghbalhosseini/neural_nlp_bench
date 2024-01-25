59e6e96cecf1b70fa6dc4b7be5bca9b31c9b77a1
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2024-01-25 18:28:05 -0500

    adding new ceiling

--- bash/ANNSet1/om_estimate_ceiling_neural_nlp_2022.sh
+++ bash/ANNSet1/om_estimate_ceiling_neural_nlp_2022.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=ceiling
-#SBATCH --array=0-1
-#SBATCH --time=36:00:00
+#SBATCH --array=0-38
+#SBATCH --time=24:00:00
 #SBATCH --mem=16G
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
@@ -13,44 +13,43 @@ num_bootstrap_samples=50
 
 i=0
 for benchmark in ANNSet1fMRI-wordForm-encoding \
-                ANNSet1fMRI-encoding ; do
-#    ANNSet1ECoG-bip-gaus-Encoding \
-#    ANNSet1ECoG-bip-gaus-strict-Encoding \
-#    ANNSet1ECoG-bip-band-Encoding \
-#    ANNSet1ECoG-bip-band-strict-Encoding \
-#    ANNSet1ECoG-uni-gaus-Encoding \
-#    ANNSet1ECoG-uni-gaus-strict-Encoding \
-#    ANNSet1ECoG-uni-band-Encoding \
-#    ANNSet1ECoG-bip-gaus-shared-LangLoc-Encoding \
-#    ANNSet1ECoG-bip-gaus-shared-LangLoc-strict-Encoding \
-#    ANNSet1ECoG-bip-band-shared-LangLoc-Encoding \
-#    ANNSet1ECoG-bip-band-shared-LangLoc-strict-Encoding \
-#    ANNSet1ECoG-uni-gaus-shared-LangLoc-Encoding \
-#    ANNSet1ECoG-uni-gaus-shared-LangLoc-strict-Encoding \
-#    ANNSet1ECoG-uni-band-shared-LangLoc-Encoding \
-#    ANNSet1ECoG-uni-band-shared-LangLoc-strict-Encoding \
-#    LangLocECoG-bip-gaus-Encoding \
-#    LangLocECoG-bip-gaus-strict-Encoding \
-#    LangLocECoG-uni-gaus-Encoding \
-#    LangLocECoG-uni-gaus-strict-Encoding \
-#    LangLocECoG-bip-gaus-zs-Encoding \
-#    LangLocECoG-bip-gaus-zs-strict-Encoding \
-#    LangLocECoG-uni-gaus-zs-Encoding \
-#    LangLocECoG-uni-gaus-zs-strict-Encoding \
-#    LangLocECoG-bip-band-Encoding \
-#    LangLocECoG-bip-band-strict-Encoding \
-#    LangLocECoG-uni-band-Encoding \
-#    LangLocECoG-uni-band-strict-Encoding \
-#    LangLocECoG-bip-gaus-shared-ANN-Encoding \
-#    LangLocECoG-bip-gaus-shared-ANN-strict-Encoding \
-#    LangLocECoG-uni-gaus-shared-ANN-Encoding \
-#    LangLocECoG-uni-gaus-shared-ANN-strict-Encoding \
-#    LangLocECoG-bip-band-shared-ANN-Encoding  \
-#    LangLocECoG-bip-band-shared-ANN-strict-Encoding \
-#    LangLocECoG-uni-band-shared-ANN-Encoding \
-#    LangLocECoG-uni-band-shared-ANN-strict-Encoding \
-
-                      benchmark_list[$i]="$benchmark"
+                ANNSet1fMRI-encoding \
+    ANNSet1ECoG-bip-gaus-Encoding \
+    ANNSet1ECoG-bip-gaus-strict-Encoding \
+    ANNSet1ECoG-bip-band-Encoding \
+    ANNSet1ECoG-bip-band-strict-Encoding \
+    ANNSet1ECoG-uni-gaus-Encoding \
+    ANNSet1ECoG-uni-gaus-strict-Encoding \
+    ANNSet1ECoG-uni-band-Encoding \
+    ANNSet1ECoG-bip-gaus-shared-LangLoc-Encoding \
+    ANNSet1ECoG-bip-gaus-shared-LangLoc-strict-Encoding \
+    ANNSet1ECoG-bip-band-shared-LangLoc-Encoding \
+    ANNSet1ECoG-bip-band-shared-LangLoc-strict-Encoding \
+    ANNSet1ECoG-uni-gaus-shared-LangLoc-Encoding \
+    ANNSet1ECoG-uni-gaus-shared-LangLoc-strict-Encoding \
+    ANNSet1ECoG-uni-band-shared-LangLoc-Encoding \
+    ANNSet1ECoG-uni-band-shared-LangLoc-strict-Encoding \
+    LangLocECoG-bip-gaus-Encoding \
+    LangLocECoG-bip-gaus-strict-Encoding \
+    LangLocECoG-uni-gaus-Encoding \
+    LangLocECoG-uni-gaus-strict-Encoding \
+    LangLocECoG-bip-gaus-zs-Encoding \
+    LangLocECoG-bip-gaus-zs-strict-Encoding \
+    LangLocECoG-uni-gaus-zs-Encoding \
+    LangLocECoG-uni-gaus-zs-strict-Encoding \
+    LangLocECoG-bip-band-Encoding \
+    LangLocECoG-bip-band-strict-Encoding \
+    LangLocECoG-uni-band-Encoding \
+    LangLocECoG-uni-band-strict-Encoding \
+    LangLocECoG-bip-gaus-shared-ANN-Encoding \
+    LangLocECoG-bip-gaus-shared-ANN-strict-Encoding \
+    LangLocECoG-uni-gaus-shared-ANN-Encoding \
+    LangLocECoG-uni-gaus-shared-ANN-strict-Encoding \
+    LangLocECoG-bip-band-shared-ANN-Encoding  \
+    LangLocECoG-bip-band-shared-ANN-strict-Encoding \
+    LangLocECoG-uni-band-shared-ANN-Encoding \
+    LangLocECoG-uni-band-shared-ANN-strict-Encoding ; do
+   benchmark_list[$i]="$benchmark"
                       i=$[$i+1]
 done
 
@@ -73,4 +72,4 @@ conda activate neural_nlp_2022
 which python
 
 
-/om/weka/evlab/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /om2/user/ehoseini/neural-nlp-2022/compute_ceiling_for_benchmark.py --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}" --num_subsamples $num_subsamples --num_bootstraps $num_bootstrap_samples
+/om/weka/evlab/ehoseini/miniconda3/envs/neural_nlp_2022/bin/python /weka/scratch/weka/evlab/ehoseini/neural-nlp-2022/compute_ceiling_for_benchmark.py --benchmark "${benchmark_list[$SLURM_ARRAY_TASK_ID]}" --num_subsamples $num_subsamples --num_bootstraps $num_bootstrap_samples
