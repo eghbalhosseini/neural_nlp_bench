b681b29cbf5a78c27bf06a12cee71411811a2b10
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2024-02-21 09:37:20 -0500

    adding new ceiling

--- bash/DsParametric/om_run_ANN_models_on_DsParametric_neural_nlp_2022.sh
+++ bash/DsParametric/om_run_ANN_models_on_DsParametric_neural_nlp_2022.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=nlp2022
-#SBATCH --array=0-132
-#SBATCH --time=24:00:00
+#SBATCH --array=0-65
+#SBATCH --time=4:00:00
 #SBATCH -c 16
-#SBATCH --mem=40G
+#SBATCH --mem=20G
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
 
 i=0
-for benchmark in DsParametricfMRI-max-encoding DsParametricfMRI-min-encoding DsParametricfMRI-rand-encoding \
-  DsParametricfMRI_v1_max-RidgeEncoding DsParametricfMRI_v1-min-RidgeEncoding DsParametricfMRI_v1-rand-RidgeEncoding \
-  DsParametricfMRI_v2_max-RidgeEncoding DsParametricfMRI_v2-min-RidgeEncoding DsParametricfMRI_v2-rand-RidgeEncoding \
-  DsParametricfMRI_v3_max-RidgeEncoding DsParametricfMRI_v3-min-RidgeEncoding DsParametricfMRI_v3-rand-RidgeEncoding ; do
+for benchmark in DsParametricfMRI-first-max-Encoding DsParametricfMRI-first-min-Encoding DsParametricfMRI-first-rand-Encoding \
+  DsParametricfMRI-first-max-RidgeEncoding DsParametricfMRI-first-min-RidgeEncoding DsParametricfMRI-first-rand-RidgeEncoding  ; do
   for model in bert-large-uncased-whole-word-masking \
                xlnet-large-cased \
                roberta-base \
